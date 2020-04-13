e131bdda8aae6879f5f1cbf67c6ef3c8774a1d88
Gabriel Dianu <gabriel_dianu@trimble.com>
2020-04-13 08:37:53 +0300

    Initial Commit

---
+++ GitTrainingConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTrainingConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
